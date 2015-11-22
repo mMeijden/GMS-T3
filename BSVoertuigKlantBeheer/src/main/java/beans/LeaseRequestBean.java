062274db09925d560f81a983d91a155b47550d4e
Matthijs van der Meijden <mmjsvandermeijden@gmail.com>
2015-11-22 15:10:35 +0100

    customer update #repo added

---
+++ BSVoertuigKlantBeheer/src/main/java/beans/LeaseRequestBean.java
@@ -0,0 +1,31 @@
+package beans;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import persist.Customer;
+import repo.CustomerRepository;
+
+import javax.ejb.Stateful;
+import javax.inject.Inject;
+import java.io.Serializable;
+
+/**
+ * Created by @author Matthijs van der Meijden on 19-11-2015.
+ */
+@Getter
+@Setter
+@Stateful
+public class LeaseRequestBean implements Serializable{
+
+    @Inject
+    private CustomerRepository customerRepository;
+
+
+    public void addCustomer(Customer customer){
+        customerRepository.add(customer);
+        customerRepository.save();
+
+    }
+
+}
---
+++ FrontEndGMS/src/main/java/service/customerService.java
@@ -0,0 +1,7 @@
+package service;
+
+/**
+ * Created by @author Matthijs van der Meijden on 22-11-2015.
+ */
+public class customerService {
+}
